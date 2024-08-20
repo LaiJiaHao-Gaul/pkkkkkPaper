cd778e68a0b315acd08d3ca41164919f33a0c78f
赖嘉豪 <787071638@qq.com>
2024-08-20 22:56:21 +0800

    只显示最后一次测试的学习风格

--- src/backend/controllers/learningStyleController.js
+++ src/backend/controllers/learningStyleController.js
@@ -29,39 +29,9 @@ exports.saveLearningStyle = async (req, res) => {
 // 获取学习风格
 exports.getLearningStyle = async (req, res) => {
     const { username } = req.params;
-    console.log('username', username);
     let users = await db.query('SELECT id FROM Users WHERE username = ?', [username]);
-    console.log('users', users[0][0].id);
     let id = users[0][0].id;
-    let LearningStyles =await db.query('SELECT visual_score, aural_score, read_write_score, kinaesthetic_score FROM LearningStyles WHERE user_id = ?', [id]);
-    console.log('LearningStyles', LearningStyles[0][0])
-    res.json(LearningStyles[0][0]);
-    // (err, results) => {
-    //     console.log('results', results);
-    //     if (results.length > 0) {
-    //         console.log('results', results[0]);
-    //         res.json(results[0]);
-    //     } else {
-    //         res.status(404).json({ error: 'Learning style not found' });
-    //     }
-    // }
-    // , (err, users) => {
-    //     console.log('users', users);
-
-    //     // const user_id = users[0].id;
-    //     // console.log('user_id', user_id);
-    //     // db.query('SELECT visual_score, aural_score, read_write_score, kinaesthetic_score FROM LearningStyles WHERE user_id = ?', [user_id], (err, results) => {
-    //     //     console.log('results', results[0]);
-    //     //     if (err) {
-    //     //         console.error('Failed to retrieve learning style:', err);
-    //     //         return res.status(500).json({ error: 'Failed to retrieve learning style' });
-    //     //     }
-    //     //     if (results.length > 0) {
-    //     //         console.log('results', results[0]);
-    //     //         res.json(results[0]);
-    //     //     } else {
-    //     //         res.status(404).json({ error: 'Learning style not found' });
-    //     //     }
-    //     // });
-    // }
+    let LearningStyles = await db.query('SELECT visual_score, aural_score, read_write_score, kinaesthetic_score FROM LearningStyles WHERE user_id = ?', [id]);
+    console.log('res.json(LearningStyles[0][0]):', LearningStyles[0][LearningStyles[0].length-1]);
+    res.json(LearningStyles[0][LearningStyles[0].length-1]);
 };
